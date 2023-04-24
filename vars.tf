2d60118131df630484ba707ace8c777a07f30a1b
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-24 16:26:03 +0000

    mon24

--- vars.tf
+++ vars.tf
@@ -7,12 +7,12 @@ variable "jobs" {
 default = [
 
   { name = "roboshop", folder = "infra", repo_url = "https://github.com/Preetam6126/roboshop-infra" },
-  { name = "frontend", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" },
-  { name = "catalogue", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" },
-  { name = "user", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" },
-  { name = "cart", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" },
-  { name = "shipping", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" },
-  { name = "payment", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" }
+  { name = "frontend", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/frontend" },
+  { name = "catalogue", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/catalogue" },
+  { name = "user", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/user" },
+  { name = "cart", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/cart" },
+  { name = "shipping", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/shipping" },
+  { name = "payment", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/payment" }
   
   ]
 }
