bf6fee768520283b1b0c3197bffe1cdd375cddd9
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-24 13:23:11 +0000

    mon24

--- vars.tf
+++ vars.tf
@@ -1,10 +1,18 @@
 variable "folders" {
-  default = [ "infra" ]
+  default = [ "infra", "CI-pipeline" ]
   
  }
  
 variable "jobs" {
 default = [
+
   { name = "roboshop", folder = "infra", repo_url = "https://github.com/Preetam6126/roboshop-infra" }
+  { name = "frontend", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" }
+  { name = "catalogue", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" }
+  { name = "user", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" }
+  { name = "cart", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" }
+  { name = "shipping", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" }
+  { name = "payment", folder = "CI-pipeline", repo_url = "https://github.com/Preetam6126/roboshop-infra" }
+  
   ]
 }
