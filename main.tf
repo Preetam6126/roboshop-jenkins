e1f8a09d3e4608221bb63c347250f88d868d80ee
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-05-06 11:13:21 +0000

    all is well

--- main.tf
+++ main.tf
@@ -4,39 +4,41 @@ resource "jenkins_folder" "folders" {
 }
 
 resource "jenkins_job" "s-job" {
-  depends_on= [jenkins_folder.folders]
-  count     = length(var.s-jobs)
-  name      = lookup(element(var.s-jobs, count.index), "name", null)
-  folder    = "/job/${lookup(element(var.s-jobs, count.index), "folder", null)}"
-  
-  template  = templatefile("${path.module}/sb-job.xml", {
-    repo_url = "/job/${lookup(element(var.s-jobs, count.index), "repo_url", null)}"
+  depends_on = [jenkins_folder.folders]
+
+  count  = length(var.s-jobs)
+  name   = lookup(element(var.s-jobs, count.index), "name", null)
+  folder = "/job/${lookup(element(var.s-jobs, count.index), "folder", null)}"
+
+  template = templatefile("${path.module}/sb-job.xml", {
+    repo_url = lookup(element(var.s-jobs, count.index), "repo_url", null)
+    name     = lookup(element(var.s-jobs, count.index), "name", null)
   })
 
-lifecycle {
-  ignore_changes = [ template ]
-  
+  lifecycle {
+    ignore_changes = [template]
   }
 
 }
-
 
 resource "jenkins_job" "m-job" {
-  depends_on= [jenkins_folder.folders]
-  count     = length(var.m-jobs)
-  name      = lookup(element(var.m-jobs, count.index), "name", null)
-  folder    = "/job/${lookup(element(var.m-jobs, count.index), "folder", null)}"
-  
-  template  = templatefile("${path.module}/mb-job.xml", {
+  depends_on = [jenkins_folder.folders]
+
+  count  = length(var.m-jobs)
+  name   = lookup(element(var.m-jobs, count.index), "name", null)
+  folder = "/job/${lookup(element(var.m-jobs, count.index), "folder", null)}"
+
+  template = templatefile("${path.module}/mb-job.xml", {
     repo_url = lookup(element(var.m-jobs, count.index), "repo_url", null)
+    name     = lookup(element(var.m-jobs, count.index), "name", null)
   })
 
-lifecycle {
-  ignore_changes = [ template ]
-  
+ lifecycle {
+    ignore_changes = [template]
   }
 
 }
+
 
 data "aws_instance" "jenkins" {
   
